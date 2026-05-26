d7f84941fa0fefb72f7921c3ca69354ec138b88e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-26 14:54:14 +0200

    .

--- calqlatr/ResultChecker.cpp
+++ calqlatr/ResultChecker.cpp
@@ -6,17 +6,26 @@ ResultChecker::ResultChecker() {
 ResultChecker::~ResultChecker() {
 }
 
+static int WinCheck(QChar item1, QChar item2, QChar item3) {
+    if (item1 == ' ')
+        return 0; // not filled out yet
+
+    if ((item1 == item2) && (item1 == item3)) {
+        return item1 == 'x' ? 1 : -1;
+    }
+
+    return 0;
+}
+
 int ResultChecker::Check(QString cells) {
     // check for horizontal win
     for (int row = 0; row < 3; row++) {
         QChar item1 = cells[3*row + 0];
         QChar item2 = cells[3*row + 1];
         QChar item3 = cells[3*row + 2];
-        if (item1 != ' ') {
-            if ((item1 == item2) && (item1 == item3)) {
-                return item1 == 'x' ? 1 : -1;
-            }
-        }
+        int win = WinCheck(item1, item2, item3);
+        if (win)
+            return win;
     }
 
     // check for vertical win
@@ -24,13 +33,28 @@ int ResultChecker::Check(QString cells) {
         QChar item1 = cells[3*0 + col];
         QChar item2 = cells[3*1 + col];
         QChar item3 = cells[3*2 + col];
-        if (item1 != ' ') {
-            if ((item1 == item2) && (item1 == item3)) {
-                return item1 == 'x' ? 1 : -1;
-            }
-        }
+        int win = WinCheck(item1, item2, item3);
+        if (win)
+            return win;
     }
 
-    // TODO: Add diagonal check
+    // check for diagonal win
+    {
+        QChar item1 = cells[3*0 + 0];
+        QChar item2 = cells[3*1 + 1];
+        QChar item3 = cells[3*2 + 2];
+        int win = WinCheck(item1, item2, item3);
+        if (win)
+            return win;
+    }
+    {
+        QChar item1 = cells[3*0 + 2];
+        QChar item2 = cells[3*1 + 1];
+        QChar item3 = cells[3*2 + 0];
+        int win = WinCheck(item1, item2, item3);
+        if (win)
+            return win;
+    }
+
     return 0;
 }
