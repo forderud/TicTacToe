0639fafd2d3d883a5c93ad0a77248a72b21db173
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-26 17:03:40 +0200

    start on Android, Linux support

--- calqlatr/ResultMgr.cpp
+++ calqlatr/ResultMgr.cpp
@@ -12,7 +12,11 @@ ResultChecker::ResultChecker() {
     m_lib = LoadLibraryW(L"ResultChecker.dll");
     m_func_ptr = (decltype(&CheckForWin))GetProcAddress(m_lib, "CheckForWin");
 #else
-    m_lib = dlopen("ResultChecker.framework/ResultChecker", RTLD_LAZY);
+  #ifdef __APPLE__
+    m_lib = dlopen("ResultChecker.framework/ResultChecker", RTLD_LAZY); // macOS, iOS
+  #else
+    m_lib = dlopen("libResultChecker.so", RTLD_LAZY); // Linux, Android
+  #endif
     m_func_ptr = (decltype(&CheckForWin))dlsym(m_lib, "CheckForWin");
 #endif
 }
