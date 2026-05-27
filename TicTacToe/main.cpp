6a11b60a1b0b0f2709c1a517aceecd25c40d210a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-27 14:08:27 +0200

    .

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -4,7 +4,7 @@
 
 
 int main(int argc, char *argv[]) {
-    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setOrganizationName("forderud");
     QCoreApplication::setApplicationName("TicTacToe");
 
     QGuiApplication app(argc, argv);
