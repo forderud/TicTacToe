57dc9bf32b1bd08c987263a2bdf2d91a09de2065
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-06-01 14:12:00 +0200

    .

--- LibMetadata.hpp
+++ LibMetadata.hpp
@@ -8,14 +8,12 @@ struct __attribute__((packed)) LibMetadataT {
     char    name[16];
     char    description[64];
     uint8_t version[4];
-    uint8_t diagnostic : 1;
     uint8_t trusted : 1;
 
     void Print() const {
         printf("  Library Name: %s\n", name);
         printf("  Description: %s\n", description);
         printf("  Version: %u.%u.%u.%u\n", version[0], version[1], version[2], version[3]);
-        printf("  Diagnostic: %s\n", diagnostic ? "Yes" : "No");
         printf("  Trusted: %s\n", trusted ? "Yes" : "No");
     }
 };
@@ -29,10 +27,9 @@ __attribute__ ((visibility("default")))
 __attribute__ ((section("__TEXT,LibMetadata")))
 #endif
 extern const LibMetadataT LibMetadata = {
-    "MySharedLib",
-    "Sample shared library.",
-    {1, 2, 3, 4},
-    false,
+    "ResultChecker",
+    "TicTacToe result checker lib.",
+    {1, 0, 0, 0},
     true
 };
 
